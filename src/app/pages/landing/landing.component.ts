4a57dda2062affbaf2a726a2f53688bfbc9b7507
Prahalad Belavadi <github@belavadi.com>
2020-08-31 14:49:34 +0530

    add private hospital pricing

--- src/app/pages/landing/landing.component.ts
+++ src/app/pages/landing/landing.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./landing.component.scss'],
 })
 export class LandingComponent implements OnInit {
-  public billingData: BillingData[] = BillingData;
+  public private_billingData: BillingData[] = PrivateBillingData;
 
   constructor() {}
 
@@ -22,7 +22,7 @@ interface BillingData {
   suiteWard: number;
 }
 
-const BillingData: any[] = [
+const PrivateBillingData: any[] = [
   {
     requirement: 'Monitoring in Hospital',
     admissionCategory: 'Covid Routine',
@@ -52,7 +52,42 @@ const BillingData: any[] = [
     admissionCategory: 'Covid ICU with ventilator',
     generalWard: '25000',
     twinSharing: '27500',
-    privateWard: '31,250',
+    privateWard: '31250',
+    suiteWard: 'As per Hospital Tariff',
+  },
+];
+
+const PublicBillingData: any[] = [
+  {
+    requirement: 'Monitoring in Hospital',
+    admissionCategory: 'Covid Routine',
+    generalWard: '10000',
+    twinSharing: '11000',
+    privateWard: '12500',
+    suiteWard: 'As per Hospital Tariff',
+  },
+  {
+    requirement: 'Pre ICU Severity',
+    admissionCategory: 'Covid HDU / Oxygen flow',
+    generalWard: '12000',
+    twinSharing: '13000',
+    privateWard: '15000',
+    suiteWard: 'As per Hospital Tariff',
+  },
+  {
+    requirement: 'ICU Level',
+    admissionCategory: 'Covid ICU without ventilator',
+    generalWard: '15000',
+    twinSharing: '16500',
+    privateWard: '18750',
+    suiteWard: 'As per Hospital Tariff',
+  },
+  {
+    requirement: 'Ventilator',
+    admissionCategory: 'Covid ICU with ventilator',
+    generalWard: '25000',
+    twinSharing: '27500',
+    privateWard: '31250',
     suiteWard: 'As per Hospital Tariff',
   },
 ];
