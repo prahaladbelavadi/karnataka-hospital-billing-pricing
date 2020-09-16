716da5c4e149fdca9a1cf716c6b146a6072fd5fc
Prahalad Belavadi <github@belavadi.com>
2020-09-16 14:08:28 +0530

    add pubic and private pricing

--- src/app/pages/landing/landing.component.ts
+++ src/app/pages/landing/landing.component.ts
@@ -7,15 +7,24 @@ import { Component, OnInit } from '@angular/core';
 export class LandingComponent implements OnInit {
   numberOfDaysAtHospital;
   bedType;
-  bedTypes = obj;
+  privateBedTypes = privatePricing;
+  publicBedTypes = publicPricing;
+
   constructor() {}
 
   ngOnInit(): void {}
 }
 
-const obj = {
+const privatePricing = {
   'General Bed': 10000,
   HDU: 12000,
   'Isolation ICU with ventilator': 25000,
   'Isolation ICU without ventilator': 15000,
+};
+
+const publicPricing = {
+  'General Bed': 5200,
+  HDU: 7000,
+  'Isolation ICU with ventilator': 8500,
+  'Isolation ICU without ventilator': 10000,
 };
