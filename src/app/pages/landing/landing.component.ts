f1995740474ea22ade852a3c7135e6e58b5b3536
Prahalad Belavadi <github@belavadi.com>
2020-09-16 13:22:22 +0530

    form working

--- src/app/pages/landing/landing.component.ts
+++ src/app/pages/landing/landing.component.ts
@@ -1,87 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-import { BillingData } from '../../interfaces/billingData';
-
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.component.html',
   styleUrls: ['./landing.component.scss'],
 })
 export class LandingComponent implements OnInit {
-  public private_billingData: BillingData[] = PrivateBillingData;
-  public public_billingData: BillingData[] = PublicBillingData;
+  numberOfDaysAtHospital;
   bedType;
-  severity;
+  bedTypes = obj;
   constructor() {}
 
   ngOnInit(): void {}
 }
 
-const PrivateBillingData: any[] = [
-  {
-    requirement: 'Monitoring in Hospital',
-    admissionCategory: 'Covid Routine',
-    generalWard: '10000',
-    twinSharing: '11000',
-    privateWard: '12500',
-    suiteWard: 'As per Hospital Tariff',
-  },
-  {
-    requirement: 'Pre ICU Severity',
-    admissionCategory: 'Covid HDU / Oxygen flow',
-    generalWard: '12000',
-    twinSharing: '13000',
-    privateWard: '15000',
-    suiteWard: 'As per Hospital Tariff',
-  },
-  {
-    requirement: 'ICU Level',
-    admissionCategory: 'Covid ICU without ventilator',
-    generalWard: '15000',
-    twinSharing: '16500',
-    privateWard: '18750',
-    suiteWard: 'As per Hospital Tariff',
-  },
-  {
-    requirement: 'Ventilator',
-    admissionCategory: 'Covid ICU with ventilator',
-    generalWard: '25000',
-    twinSharing: '27500',
-    privateWard: '31250',
-    suiteWard: 'As per Hospital Tariff',
-  },
-];
-
-const PublicBillingData: any[] = [
-  {
-    requirement: 'Monitoring in Hospital',
-    admissionCategory: 'Covid Routine',
-    generalWard: '5200',
-    twinSharing: '11000',
-    privateWard: '12500',
-    suiteWard: 'As per Hospital Tariff',
-  },
-  {
-    requirement: 'Pre ICU Severity',
-    admissionCategory: 'Covid HDU / Oxygen flow',
-    generalWard: '7000',
-    twinSharing: '13000',
-    privateWard: '15000',
-    suiteWard: 'As per Hospital Tariff',
-  },
-  {
-    requirement: 'ICU Level',
-    admissionCategory: 'Covid ICU without ventilator',
-    generalWard: '15000',
-    twinSharing: '16500',
-    privateWard: '18750',
-    suiteWard: 'As per Hospital Tariff',
-  },
-  {
-    requirement: 'Ventilator',
-    admissionCategory: 'Covid ICU with ventilator',
-    generalWard: '25000',
-    twinSharing: '27500',
-    privateWard: '31250',
-    suiteWard: 'As per Hospital Tariff',
-  },
-];
+const obj = {
+  'General Bed': 10000,
+  HDU: 12000,
+  'Isolation ICU with ventilator': 25000,
+  'Isolation ICU without ventilator': 15000,
+};
